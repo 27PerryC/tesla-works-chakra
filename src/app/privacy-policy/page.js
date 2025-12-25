f86e6a86763e572a899615c4e837af1552f9e7d3
Carson Perry <perry884@umn.edu>
2025-12-25 02:28:29 -0600

    Theme color update

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -4,7 +4,7 @@ import Content from './content.mdx'
 
 export default function PrivacyPage() {
   return (
-    <Box bg="cream.50" minH="100vh">
+    <Box bg="light.100" minH="100vh">
       <Container maxW="container.xl" py={{ base: 10, md: 16 }}>
         <Box className="mdx">
           <Content />
